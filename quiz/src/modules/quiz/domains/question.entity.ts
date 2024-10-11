2c040bebd552e4ab9b484189b0615b2ec20ee9be
Bich Huyen <bichhuyenn151@gmail.com>
2024-10-12 02:19:29 +0700

    feat: add question entity

---
+++ quiz/src/modules/quiz/domains/question.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { QuizEntity } from "./quiz.entity";
+
+  
+  @Entity({ name: 'question' })  //({objcect key: 'object value'})
+  export class QuestionEntity {
+    @PrimaryGeneratedColumn()
+    id: string 
+
+    @Column()
+    content: string;
+    
+    @ManyToOne(()=>QuizEntity)
+    @JoinColumn({name: 'quiz_id'})
+    quizEntity:QuizEntity
+}
+
